a213d89051dfbd24a674ae062633111c049e1ac2
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-01-29 12:53:13 +0100

    Update bucleswhile.py

--- bucleswhile.py
+++ bucleswhile.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 numero=input("Escriba un número:")
-numeromayor=input("Escriba un número mayor que:")
+numeromayor=input("Escriba un número mayor:")
 while (numeromayor>numero):
 	numeromayor=input("Intenta de nuevo:")
 print "Numeros que has escrito:", numero, "y", numeromayor
