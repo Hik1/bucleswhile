f96cf18b9b4383bb99fd6aca03f19d24bfa91aab
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-01-29 13:12:55 +0100

    Add files via upload

---
+++ Buclewhile1-1.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+numero=input("Escriba un número:")
+numeromayor=input("Escriba un número mayor:")
+while (numero>=numeromayor):
+	numeromayor=input("No es mayor, intente de nuevo:")
+print "Numeros que has escrito son:", numero, "y", numeromayor
+	
+	
+
+		
