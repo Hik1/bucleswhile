912bf5e70987eabeb8ee0084e3471922fa853487
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-01-29 13:37:54 +0100

    Update Bucle while 1-2.py

--- Bucle while 1-2.py
+++ Bucle while 1-2.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 numero=float(input("Escriba un número:"))
 numeromayor=float(input("Escriba un número mayor:"))
-while (numero>numeromayor):
+while (numero<numeromayor):
 	numeromayor=input("Intente de nuevo:")
 print "Numeros escritos:", numero, "y", numeromayor	
