5aad837e56c299b1aadb40d87803e4fa42731090
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-02-01 12:28:12 +0100

    Update nveces_01.py

--- nveces_01.py
+++ nveces_01.py
@@ -2,6 +2,9 @@
 num1=input("Introduce un numero:")
 num2=0
 while (num2<num1):
-	num2+=1
-	print (input("Dime numero:"))
+	num2=num2+1
+	aux=input("Introduce otro numero:")
+	if aux<0:
+		num2=num2-1
+print "Programa terminado"
 
