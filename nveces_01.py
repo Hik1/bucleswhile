1fe41d51d835ad3222ccfbc74f6bdae4e327cb1c
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-02-01 12:57:20 +0100

    Update nveces_01.py

--- nveces_01.py
+++ nveces_01.py
@@ -8,3 +8,13 @@ while (num2<num1):
 		num2=num2-1
 print "Programa terminado"
 
+num1=input("Introduce un numero:")
+num2=0
+suma=0
+while (num2<num1):
+	aux=input("Introduce otro numero:")
+ 	if aux>0:
+		num2=num2+1
+		suma=suma+num1
+print "La suma es:", str(suma) + "."
+
