119750fbe3b92ba8c063579e2df0b316b3800c83
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-02-01 13:49:48 +0100

    Add files via upload

---
+++ Bucle while 1-5.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+num1=input("Escriba un número: ")
+suma=0
+while num1>0:
+    suma+=num1
+    num1=input("Escriba otro número: ")
+print "La suma es:", str(suma) + "."
+print "Programa terminado"
