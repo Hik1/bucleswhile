cd9a65f6e2918946dcdd6f4045181a59ea0a5b4b
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-02-05 12:31:53 +0100

    Update Bucle while 1-6.py

--- Bucle while 1-6.py
+++ Bucle while 1-6.py
@@ -1,11 +1,12 @@
 #coding: utf-8
-num1=float(input("Escriba el limite: "))
-while num1<= 0:
+num1=float(input("Escriba un limite: "))
+while num1<=0:
+	num1=float(input("Debe ser mayor que 0 intente de nuevo:"))
 num2=float(input("Escriba un número: "))
 suma=0
 suma+=num2
 while suma<num1:
     num2=float(input("Escriba otro número: "))
     suma+=num2
-print("Suma de números positivos:", str(suma)+".")
-print("Programa terminado")
+print "Se ha superado el limite, la suma de los numeros positivos es:", str(suma) + "."
+print "Programa terminado"
