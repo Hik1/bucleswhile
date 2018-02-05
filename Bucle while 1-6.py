af4ffef6cb11354b410b6c1b1a20202dc8926dda
Hik1 <35691523+Hik1@users.noreply.github.com>
2018-02-05 12:18:04 +0100

    Update Bucle while 1-6.py

--- Bucle while 1-6.py
+++ Bucle while 1-6.py
@@ -1,7 +1,6 @@
 #coding: utf-8
 num1=float(input("Escriba el limite: "))
 while num1<= 0:
-    num1=float(input("El limite debe ser mayor que 0. Intente de nuevo: "))
 num2=float(input("Escriba un número: "))
 suma=0
 suma+=num2
